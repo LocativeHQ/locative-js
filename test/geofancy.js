273d1b5c5d9f4b35d51c91f81c0622c26e358e35
tbasse <himself@tagedieb.com>
2014-01-19 13:25:41 +0100

    fix test for version number

--- test/geofancy.js
+++ test/geofancy.js
@@ -6,6 +6,6 @@ describe('Geofancy', function () {
   it('should know its version', function () {
     var client = Geofancy;
     expect(client.version).to.not.equal(undefined);
-    expect(client.version).to.equal('0.0.1');
+    expect(client.version).to.match(/^\d\.\d\.\d$/);
   });
 });

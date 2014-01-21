6fec19d74f5f9646c63fe775ee86133d521eac18
tbasse <himself@tagedieb.com>
2014-01-21 17:14:57 +0100

    add test for getFencelogs()

--- test/geofancy.js
+++ test/geofancy.js
@@ -2,6 +2,21 @@ var mocha  = require('mocha');
 var expect = require('chai').expect;
 var Geofancy = require('../index');
 
+var mochaTestApp = {
+  client_id: 'vNFnaPmmbNMVnP1jUSxgLgVSvgoV16ph',
+  client_secret: 'ftxuRw8vsSGGXBJ7RJXGgkeD3yC5UZCy',
+  client_accesstoken: 't6GFVlNLIiyC95RNKkpMN8Vqfo1pTd2wV7IyCYXRmkHEUWz6tSPxeueoiCs1OfhO',
+  redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
+  grant_type: 'code'
+};
+
+var client = new Geofancy.Client({
+  client_id: mochaTestApp.client_id,
+  client_secret: mochaTestApp.client_secret,
+  redirect_uri: mochaTestApp.redirect_uri,
+  grant_type: mochaTestApp.grant_type
+});
+
 describe('Geofancy', function () {
 
   it('is a function', function () {
@@ -25,32 +40,14 @@ describe('Geofancy.Client', function () {
   });
 
   it('has a method authorizeUrl', function () {
-    var client = new Geofancy.Client({
-      client_id: 'client_id',
-      client_secret: 'client_secret',
-      redirect_uri: 'redirect_uri',
-      grant_type: 'code'
-    });
     expect(client.authorizeUrl).to.be.an.instanceOf(Function);
   });
 
   it('has a method getToken', function () {
-    var client = new Geofancy.Client({
-      client_id: 'client_id',
-      client_secret: 'client_secret',
-      redirect_uri: 'redirect_uri',
-      grant_type: 'code'
-    });
     expect(client.getToken).to.be.an.instanceOf(Function);
   });
 
   it('has a method getFencelogs', function () {
-    var client = new Geofancy.Client({
-      client_id: 'client_id',
-      client_secret: 'client_secret',
-      redirect_uri: 'redirect_uri',
-      grant_type: 'code'
-    });
     expect(client.getFencelogs).to.be.an.instanceOf(Function);
   });
 
@@ -67,62 +64,37 @@ describe('Geofancy.version', function () {
 describe('Geofancy.Client.authorizeUrl()', function () {
 
   it('returns a string', function () {
-    var client = new Geofancy.Client({
-      client_id: 'client_id',
-      client_secret: 'client_secret',
-      redirect_uri: 'redirect_uri',
-      grant_type: 'code'
-    });
     expect(client.authorizeUrl()).to.be.a('string');
   });
 
   it('returns correct authorizeUrl for given id and redirect_uri', function () {
-    var client = new Geofancy.Client({
-      client_id: 'client_id',
-      client_secret: 'client_secret',
-      redirect_uri: 'redirect_uri',
-      grant_type: 'code'
-    });
     expect(client.authorizeUrl()).to.equal(
-      'https://my.geofancy.com/oauth2/authorize?client_id=client_id&response_type=code&redirect_uri=redirect_uri'
+      'https://my.geofancy.com/oauth2/authorize?client_id=' +
+      mochaTestApp.client_id +
+      '&response_type=code&redirect_uri=' +
+      mochaTestApp.redirect_uri
     );
   });
 
 });
 
-/*
-describe('Geofancy.Client.getToken()', function () {
+// describe('Geofancy.Client.getToken()', function () {
+//   it('returns accessToken', function (done) {
+//     console.log(client.authorizeUrl());
+//     client.getToken(mochaTestApp.client_accesstoken, function(accessToken) {
+//       if (accessToken) {
+//         done();
+//       } else {
+//         throw new Error('returned ' + typeof accessToken);
+//       }
+//     });
+//   });
+// });
 
-  it('returns accessToken', function (done) {
-    var client = new Geofancy.Client({
-      client_id: 'client_id',
-      client_secret: 'client_secret',
-      redirect_uri: 'redirect_uri',
-      grant_type: 'code'
-    });
-    client.getToken('code', function(accessToken) {
-      if (accessToken) {
-        done();
-      } else {
-        throw new Error('returned ' + typeof accessToken);
-      }
-    });
-  });
-
-});
-*/
-
-/*
 describe('Geofancy.Client.getFencelogs()', function () {
 
   it('returns json', function (done) {
-    var client = new Geofancy.Client({
-      client_id: 'client_id',
-      client_secret: 'client_secret',
-      redirect_uri: 'redirect_uri',
-      grant_type: 'code'
-    });
-    client.getFencelogs('token', function(fencelogs) {
+    client.getFencelogs(mochaTestApp.client_accesstoken, function(fencelogs) {
       if (fencelogs) {
         done();
       } else {
@@ -132,4 +104,3 @@ describe('Geofancy.Client.getFencelogs()', function () {
   });
 
 });
-*/

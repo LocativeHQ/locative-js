170aaf7ace04fee0ff1c2618b0cb25dd175cf97f
Marcus Kida <marcus@kida.io>
2015-12-02 06:53:24 +1100

    Migrate to `Locative`

--- README.md
+++ README.md
@@ -1,30 +1,30 @@
-# GeofancyJS
+# Locative.js
 
-![Travis-CI Badge](https://travis-ci.org/Geofancy/geofancy-js.png)
+![Travis-CI Badge](https://travis-ci.org/LocativeHQ/locative-js.png)
 
-*The official Geofancy node.js NPM module*
+*The official Locative node.js NPM module*
 
-Have a look at the [geofancy-node-sample](https://github.com/Geofancy/geofancy-node-sample) if you want a quick demo.
+Have a look at the [nodejs-sample](https://github.com/LocativeHQ/nodejs-sample) if you want a quick demo.
 
 Install using NPM like so:
 
 ```
-npm install geofancy
+npm install locative
 ```
 
 or use
 
 ```
-npm install -S geofancy
+npm install -S locative
 ```
 
-To keep geofancy as a dependency inside your package.json
+To keep Locative as a dependency inside your package.json
 
 Then create an client instance this way:
 
 ```
-var Geofancy = require('geofancy');
-var client = new Geofancy.Client({
+var Locative = require('locative');
+var client = new Locative.Client({
     client_id: 'YOUR CLIENT ID',
     client_secret: 'YOUR CLIENT SECRET',
     redirect_uri: 'YOUR REDIRECT URI',
@@ -32,9 +32,9 @@ var client = new Geofancy.Client({
 });
 ```
 
-To retrieve an accessToken it's neccessary to, at first, retrieve and authorizationCode which can be done using the client's **authorizeUrl()** method which returns a oAuth v2 Authorization url agains the my.geofancy.com API.
+To retrieve an accessToken it's neccessary to, at first, retrieve and authorizationCode which can be done using the client's **authorizeUrl()** method which returns a oAuth v2 Authorization url agains the my.locative.io API.
 
-After authentication using the authorization Url, you will be redirected to the redirect_uri (please note that this URI must be the same URI you've entered when creating the oAuth v2 App at my.geofancy.com).
+After authentication using the authorization Url, you will be redirected to the redirect_uri (please note that this URI must be the same URI you've entered when creating the oAuth v2 App at my.locative.io).
 
 You may then exchange the authorizationCode by an accessToken using this method:
 
--- test/locative.js
+++ test/locative.js
@@ -1,6 +1,6 @@
 var mocha  = require('mocha');
 var expect = require('chai').expect;
-var Geofancy = require('../index');
+var Locative = require('../index');
 
 var mochaTestApp = {
   client_id: 'vNFnaPmmbNMVnP1jUSxgLgVSvgoV16ph',
@@ -10,33 +10,33 @@ var mochaTestApp = {
   grant_type: 'code'
 };
 
-var client = new Geofancy.Client({
+var client = new Locative.Client({
   client_id: mochaTestApp.client_id,
   client_secret: mochaTestApp.client_secret,
   redirect_uri: mochaTestApp.redirect_uri,
   grant_type: mochaTestApp.grant_type
 });
 
-describe('Geofancy', function () {
+describe('Locative', function () {
 
   it('is a function', function () {
-    expect(Geofancy.Client).to.be.an.instanceOf(Function);
+    expect(Locative.Client).to.be.an.instanceOf(Function);
   });
 
   it('has a method Client', function () {
-    expect(Geofancy.Client).to.be.an.instanceOf(Function);
+    expect(Locative.Client).to.be.an.instanceOf(Function);
   });
 
 });
 
-describe('Geofancy.Client', function () {
+describe('Locative.Client', function () {
 
   it('is a function', function () {
-    expect(Geofancy.Client).to.be.an.instanceOf(Function);
+    expect(Locative.Client).to.be.an.instanceOf(Function);
   });
 
   it('has a string version', function () {
-    expect(Geofancy.version).to.be.a('string');
+    expect(Locative.version).to.be.a('string');
   });
 
   it('has a method authorizeUrl', function () {
@@ -53,15 +53,15 @@ describe('Geofancy.Client', function () {
 
 });
 
-describe('Geofancy.version', function () {
+describe('Locative.version', function () {
 
   it('matches format 0.1.2', function () {
-    expect(Geofancy.version).to.match(/^\d\.\d\.\d$/);
+    expect(Locative.version).to.match(/^\d\.\d\.\d$/);
   });
 
 });
 
-describe('Geofancy.Client.authorizeUrl()', function () {
+describe('Locative.Client.authorizeUrl()', function () {
 
   it('returns a string', function () {
     expect(client.authorizeUrl()).to.be.a('string');
@@ -69,7 +69,7 @@ describe('Geofancy.Client.authorizeUrl()', function () {
 
   it('returns correct authorizeUrl for given id and redirect_uri', function () {
     expect(client.authorizeUrl()).to.equal(
-      'https://my.geofancy.com/oauth2/authorize?client_id=' +
+      'https://my.locative.io/oauth2/authorize?client_id=' +
       mochaTestApp.client_id +
       '&response_type=code&redirect_uri=' +
       mochaTestApp.redirect_uri
@@ -78,7 +78,7 @@ describe('Geofancy.Client.authorizeUrl()', function () {
 
 });
 
-// describe('Geofancy.Client.getToken()', function () {
+// describe('Locative.Client.getToken()', function () {
 //   it('returns accessToken', function (done) {
 //     console.log(client.authorizeUrl());
 //     client.getToken(mochaTestApp.client_accesstoken, function(accessToken) {
@@ -91,7 +91,7 @@ describe('Geofancy.Client.authorizeUrl()', function () {
 //   });
 // });
 
-describe('Geofancy.Client.getFencelogs()', function () {
+describe('Locative.Client.getFencelogs()', function () {
 
   it('returns json', function (done) {
     client.getFencelogs(mochaTestApp.client_accesstoken, function(fencelogs) {
